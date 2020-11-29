532a1030274817b392840141daa42142de983d08
Arthur <rizhiy2406@gmail.com>
2020-11-29 20:11:33 +0200

    Add mocked AdminControllers

--- server/src/controllers/AdminControllers.ts
+++ server/src/controllers/AdminControllers.ts
@@ -1,9 +1,50 @@
 import { AdminRepository } from "../repositories/AdminRepository";
+import { Request, Response, NextFunction } from "express";
+import { apiErrorHandler } from "../errors/apiErrorHandler";
+import { ItemModel } from "../models/ItemModel";
 
 export class AdminControllers {
-    private _adminRepo: AdminRepository;
+    private _repo: AdminRepository;
 
     constructor(adminRepo: AdminRepository) {
-        this._adminRepo = adminRepo;
+        this._repo = adminRepo;
     }
+
+
+    createItems = async (req: Request, res: Response, next: NextFunction) => {
+        try { 
+            const createdItems = await this._repo.createItems([
+                {
+                    name: 'iphone2',
+                    image: 'path/to/iphone2.img',
+                    description: 'overpriced',
+                    price: 255.5 
+                },
+                {
+                    name: 'iphone3',
+                    image: 'path/to/iphone3.img',
+                    description: 'overpriced shit',
+                    price: 3552.4 
+                }
+            ]);
+            res.json(createdItems);
+        } catch (err) {
+            apiErrorHandler(err, req, res, 'Unable to create items');
+        } 
+    }  
+
+
+    updateItem = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const sampleDataToUpdate = {
+                name: 'updated name',
+                image: 'updated image'
+            };
+            const updatedItem = await this._repo.updateItemById(parseInt(req.params.itemId), sampleDataToUpdate);
+            res.json(updatedItem);
+        } catch (err) {
+            apiErrorHandler(err, req, res, 'Unable to update item');
+        }
+    }
+    
 }
