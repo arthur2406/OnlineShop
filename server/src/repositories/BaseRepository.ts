24865333a37cf379de1418557ca7671cef3e5c89
Arthur <rizhiy2406@gmail.com>
2020-11-29 20:09:53 +0200

    BaseRepo: add methods to get/update/add rows

--- server/src/repositories/BaseRepository.ts
+++ server/src/repositories/BaseRepository.ts
@@ -3,11 +3,13 @@ import { DatabaseCollection } from "../enumerations/DatabaseCollection";
 import { ArgumentUndefinedOrNullError } from "../errors/ArgumentUndefinedOrNullError";
 import { Queriable } from "../interfaces/Querieable";
 import { DatabaseError } from "../errors/DatabaseError";
+import { truncateSync } from "fs";
+import { DataModel } from "../models/DataModel";
 
-export abstract class BaseRepository {
+export class BaseRepository {
 
+    /** Data source. */
     protected _db: Queriable;
-
 
     constructor(db: Queriable) {
         if (db == null) throw ArgumentUndefinedOrNullError.createErrArgNotSpec('db');
@@ -15,11 +17,15 @@ export abstract class BaseRepository {
     }
 
 
-    protected async getRows<T>(collectionName: DatabaseCollection): Promise<T[]> {
+    /** Get all rows from specified table. */
+    protected async getRows<T extends DataModel>(collectionName: DatabaseCollection, sqlCondition?: string): Promise<T[]> {
         try {
-            const db = this._db;
-            const sqlString = `SELECT * FROM ${collectionName};`;
-            const result: T[] = await db.query(sqlString);        
+            // Build sql string.
+            let sqlString = `SELECT * FROM ${collectionName} `;
+            if (sqlCondition != null) sqlString += sqlCondition;
+            sqlString += ';';
+
+            const result: T[] = await this._db.query(sqlString);        
             return result;
         } catch (err) {
             throw new DatabaseError('Unable to get rows', err);
@@ -27,10 +33,69 @@ export abstract class BaseRepository {
     }
 
 
+    /** Insert rows into the specified table. */
+    protected async addRows<T extends DataModel>(collectionName: DatabaseCollection, rowsData:T[]): Promise<T[]> {
+        try {
+
+            // Build SQL string.
+            const fields = Object.keys(rowsData[0]).join(',');
+            let valuesString: string = ''; 
+            rowsData.forEach((item: T, index: number) => {
+                let crrValString = '('; 
+    
+                crrValString += Object.values(item)
+                    .map((val: any) =>  `'${val}'`)
+                    .join(',');
+    
+                crrValString += index === rowsData.length - 1 ? ')' : '),'
+                valuesString += crrValString;
+            });
+            const sqlString = `INSERT INTO ${collectionName} (${fields}) VALUES ${valuesString} RETURNING *;`;
+
+            const result: T[] = await this._db.query(sqlString);
+
+            return result; 
+        } catch (err) {
+            throw new DatabaseError('Unable to insert rows', err);
+        }
+    }
+
+
+    /** Updates row in specified table. */
+    protected async updateRows<T extends DataModel>(collectionName: DatabaseCollection, whereCondition :string, dataToUpdate: Partial<T>): Promise<T[]> {
+        try {
+
+            // Build SQL string.
+            const rowData: string[] = [];
+            Object.entries(dataToUpdate).forEach((keyValueArr) => {
+                rowData.push(`${keyValueArr[0]}='${keyValueArr[1]}'`);
+            });
+            const setClause = rowData.join(','); 
+            const sqlString = `UPDATE ${collectionName} ` +
+                `SET ${setClause} ` +
+                `WHERE ${whereCondition} ` +
+                'RETURNING *;';
+
+
+            const result: T[] = await this._db.query(sqlString);
+            return result;
+
+        } catch (err) {
+            throw new DatabaseError('Unable to update row', err);
+        }
+    } 
+
+
+
+
+    //#region Common methods.
+    
     /** Get all rows from 'Item' table. */
     async getItems(): Promise<ItemModel[]> {
         const items: ItemModel[] = await this.getRows<ItemModel>(DatabaseCollection.Item);
         return items;
     }
+
+    //#endregion
     
 }
