79dbf0d026298dc3beeb990a391a43bef2cf0f14
Arthur <rizhiy2406@gmail.com>
2020-11-29 20:12:43 +0200

    AdminRepo: add create/update item methods

--- server/src/repositories/AdminRepository.ts
+++ server/src/repositories/AdminRepository.ts
@@ -1,8 +1,32 @@
+import { ItemModel } from "../models/ItemModel";
 import { Queriable } from "../interfaces/Querieable";
 import { BaseRepository } from "./BaseRepository";
+import { DatabaseCollection } from "../enumerations/DatabaseCollection";
+import { ArgumentUndefinedOrNullError } from "../errors/ArgumentUndefinedOrNullError";
 
 export class AdminRepository extends BaseRepository {
     constructor(db: Queriable) {
         super(db);
+    }
+
+
+    /** Inserts items into database. */
+    async createItems(itemsData: ItemModel[]): Promise<ItemModel[]> {
+        if (itemsData == null) throw ArgumentUndefinedOrNullError.createErrArgNotSpec('itemsData.');
+
+        const items: ItemModel[] = await this.addRows<ItemModel>(DatabaseCollection.Item, itemsData);
+        return items;
+    }
+
+
+    /** Updates item with specified id with passed data. */
+    async updateItemById(itemId: number, itemDataToUpdate: Partial<ItemModel>): Promise<ItemModel[]> {
+        if (itemId == null) throw ArgumentUndefinedOrNullError.createErrArgNotSpec('itemId.');
+        if (itemDataToUpdate == null) throw ArgumentUndefinedOrNullError.createErrArgNotSpec('itemDataToUpdate.');
+
+        const whereCondition = `Id = ${itemId}`;
+        const updatedItem = await this.updateRows<ItemModel>(DatabaseCollection.Item, whereCondition, itemDataToUpdate);
+        return updatedItem;
+
     }
 }
